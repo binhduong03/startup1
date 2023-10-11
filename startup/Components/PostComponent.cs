c35292253f55d7932199204bcb12d34d8401b5e0
Lê Phan Bình Dương <binhduongofficial@gmail.com>
2023-10-11 09:58:13 +0700

    3/10/2023

--- startup/Components/PostComponent.cs
+++ startup/Components/PostComponent.cs
@@ -17,7 +17,7 @@ namespace startup.Components
             var listofPost = (from p in _context.Posts
                               where (p.IsActive == true) && (p.Status == 1)
                               orderby p.PostID descending
-                              select p).Take(3).ToList();
+                              select p).Take(4).ToList();
             return await Task.FromResult((IViewComponentResult)View("Default", listofPost));
         }
     }
